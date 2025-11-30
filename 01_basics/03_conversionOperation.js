6ab5f96c986df5a0babcdd7c321343649eea4064
Aminur Rahman <ar2069413@gmail.com>
2025-11-30 10:35:13 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+     // Conversion 
+    
+let score = 66;
+console.log(typeof score);
+
+let newScore = "66";
+console.log(typeof newScore);
+
+
+let valueInNumber = Number(700);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let newNumber = String(600);
+console.log(typeof newNumber);
+console.log(newNumber);
+
+score = "59Amin";
+console.log(typeof score);
+
+
+
+let isLoggedIn = "";
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
