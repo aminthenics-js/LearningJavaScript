b2bf26a7788223b6ffce86bb2b6cf5085b1bf6a7
Aminur Rahman <ar2069413@gmail.com>
2025-12-02 12:39:40 +0000

    Comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,31 @@
+   // 1. Strict Comparison
+ 
+   console.log(5 === 5);   
+   console.log("5" === 5);   
+   console.log(true === 1);   
+   console.log(null === undefined);   
+   console.log("5" !== 5);   
+   console.log(10 !== 10);   
+
+const a = {};
+const b = a;
+console.log(a === b);  // same reference
+
+
+
+
+
+console.log();
+   // 2. Loose Comparsion
+
+   console.log("5" == 5);
+   console.log(true == 1);
+   console.log(false == 0);
+   console.log("" == false);
+   console.log(null == undefined);
+   console.log([] == []); 
+
+   console.log(null > 0);
+   console.log(null < 0);
+   console.log(null >= 0);
+   
