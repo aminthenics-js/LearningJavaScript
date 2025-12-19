49bb83667943b926fe4fe485fc4f3596d2ad10d7
Aminur Rahman <ar2069413@gmail.com>
2025-12-19 14:56:49 +0000

    json talks

--- 02_basics/04_More-About-Object.js
+++ 02_basics/04_More-About-Object.js
@@ -5,7 +5,7 @@
      discordUser.name = "Samual"
      discordUser.isLoggedIn = true
 
-     //console.log(discordUser);
+     console.log(discordUser);
      
 
      const regularUser = {
@@ -19,9 +19,9 @@
      }
 
 
-     //console.log(regularUser.fullName);
-     //console.log(regularUser.fullName.userName);
-     //console.log(regularUser.fullName.userName.lastName);
+     console.log(regularUser.fullName);
+     console.log(regularUser.fullName.userName);
+     console.log(regularUser.fullName.userName.lastName);
      
 
 
@@ -31,11 +31,11 @@
 
     
 
-//    const mergeObject = Object.assign({}, firstObject1, secondObject, thirdObject);
+   const mergeObject = Object.assign({}, firstObject1, secondObject, thirdObject);
 
-  //  console.log(mergeObject);
+   console.log(mergeObject);
     
-  const mergeObject = {...firstObject1, ...secondObject, ...thirdObject};  //Preferred
+  //const mergeObject = {...firstObject1, ...secondObject, ...thirdObject};  //Preferred
   console.log(mergeObject);
 
 
@@ -68,3 +68,120 @@
     console.log(discordUser.hasOwnProperty('isLoggedIn'));
     console.log(discordUser.hasOwnProperty('LoggedIn'));
     
+
+
+
+
+
+
+
+
+
+
+                          
+
+
+
+
+
+
+
+
+           // OBJECT DESTRUCTURING (brief introduction) :-
+
+
+const course = {
+    courseName: "JS for beginners",
+    coursePrice: "949",
+    courseInstructor: "Hitesh sir"
+};
+
+
+const {courseName} = course;
+console.log(courseName);
+
+const {courseInstructor: author} = course;
+console.log(author);
+
+
+
+
+
+
+                   //     JSON - JavaScript Object Notation
+/*
+    {
+        "name": "Aminthenics",
+        "courseName": "JavaScript course",
+        "price: "free"
+    };
+
+
+    {
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
+
+*/
