86c364fc0c2f734547600b7e838e9242af51a751
Aminur Rahman <ar2069413@gmail.com>
2025-12-21 12:08:01 +0000

    Function nad if statement

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,55 @@
+
+
+        function sayMyName(){
+            console.log("H");
+            console.log("E");
+            console.log("I");
+            console.log("S");
+            console.log("N");
+            console.log("B");
+            console.log("E");
+            console.log("R");
+            console.log("G");
+            
+        }
+
+    sayMyName()
+
+
+       function sumTwoNumbers(number1, number2){
+        console.log(number1 + number2);
+       }
+
+       sumTwoNumbers()
+       sumTwoNumbers(9, "11")
+        const result = sumTwoNumbers(9, 11);
+
+        
+        
+
+          function sumTwoNumbers(number1, number2){
+          let result = number1 + number2;
+          // console.log("Aminthenics");
+          return result
+       }
+
+       console.log("Result :", result);
+
+
+
+      function loginUserMessage(username){
+        return `${username} is logged in`
+      }
+
+      console.log(loginUserMessage());
+      
+
+         function loginUserMessage(username){
+          if(!username) {
+            console.log("Please enter a username");
+            return
+          }
+        return `${username} is logged in`
+      }
+
+      console.log(loginUserMessage("Aminthenics"));
