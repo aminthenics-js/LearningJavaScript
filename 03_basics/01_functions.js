926442466e237cf4dcc9077c01dda55b94f34c21
Aminur Rahman <ar2069413@gmail.com>
2025-12-22 14:45:04 +0000

    Funtions with object & array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -62,4 +62,38 @@
         return `You have ${numberOfItems} items in your cart`
       }
 
-      console.log(itemInCart(5))
+      console.log(itemInCart(5));
+
+
+
+
+    function calculateCartPrice(...num1){         // rest operator
+        return num1;
+    }
+
+    console.log(calculateCartPrice(100, 499, 209, 383));
+    
+
+
+     const user = {
+      name: "Aminur",
+      age: 20
+     };
+
+     function handleObject(newobject){
+        console.log(`Username is ${newobject.name} and age is ${newobject.age}`);  
+     }
+
+     handleObject({
+      name: "Rahman",
+      age: 30
+     })
+
+
+     const newArrElement = [100, 499, 209, 383];
+
+     function returnElement(array){
+        return array[3];
+     }
+
+     console.log(newArrElement);
