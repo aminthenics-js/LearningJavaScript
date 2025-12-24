83ba8da4773f242203375383527e065325cda351
Aminur Rahman <ar2069413@gmail.com>
2025-12-24 10:06:46 +0000

    Nested Function, Closures and Expression

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -14,7 +14,7 @@
  }
 
 
-    console.log("Outer: ", firstVariable);
+      console.log("Outer: ", firstVariable);
     // console.log(secondVariable);
    // console.log(thirdVariable);     // Function-scoped(AVOID)
     
@@ -25,3 +25,68 @@
     for (let i = 0; i < 10; i++) {
         console.log("Aminthenics");
     }
+
+
+
+
+
+
+
+
+
+
+         // Nested function
+
+
+
+
+      function parent(){
+         const userName = "Aminur";
+
+         function child(){
+            const website = "Github";
+            console.log(userName);
+         }
+     //    console.log(website);
+
+         child()
+      }
+
+      parent()
+
+
+
+
+      if (true) {
+         const userName = "Rahman";
+         if (userName === "Rahman") {
+            const website = " Youtube";
+            console.log(userName + website);
+         }
+      //   console.log(website);
+         
+      }
+
+   //   console.log(userName);
+      
+
+
+         console.log(addOne(7));
+      function addOne(num){
+         return num + 1;
+      }
+
+      console.log(addOne(7));
+
+
+
+
+
+               // Expression  = function stored in variable
+
+
+      const addTwo = function(num){
+         return num + 2;
+      }
+
+      console.log(addTwo(4))
